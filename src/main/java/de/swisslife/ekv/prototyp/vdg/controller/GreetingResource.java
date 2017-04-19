46e5a39caef983b73434850bb949a611a8008778
Marcel <marcel.seifert@googlemail.com>
2017-04-19 21:28:53 +0200

    Initial commit

---
+++ src/main/java/de/swisslife/ekv/prototyp/vdg/controller/GreetingResource.java
@@ -0,0 +1,20 @@
+package de.swisslife.ekv.prototyp.vdg.controller;
+
+import de.swisslife.ekv.prototyp.vdg.model.Greeting;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingResource {
+
+  private static final String template = "Hello, %s!";
+  private final AtomicLong counter = new AtomicLong();
+
+  @RequestMapping("/greeting")
+  public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    return new Greeting(counter.incrementAndGet(),
+        String.format(template, name));
+  }
+}
